b132a0e8f4b012296719b978acb12221dc8286b0
FotihYusupov <yusupovfotih158@gmai.com>
2023-12-13 10:32:21 +0500

    add remove function backed and favorites

--- pages/basked/index.vue
+++ pages/basked/index.vue
@@ -1,13 +1,13 @@
 <template>
     <h1>Savat</h1>
+    <button @click="removeAllBasked">Barchasini O'chirish</button>
     <ul class="products-list">
             <li v-for="product in basked" class="products-list__item" :key="product._id" :id="product._id">
                 <img class="products-list__item-img" :src="product.product_img[0]" width="200" height="200"/>
                 <h3>{{ product.product_title }}</h3>
                 <p>{{ product.product_desc }}</p>
                 <p v-if="product.product_price > 0"><b>Price: </b>{{ product.product_price }} so'm</p>
-                <button @click="addBasked">Savatcha</button>
-                <button @click="addFavorites">Sevimlilar</button>
+                <button @click="removeBasked">Ochirish</button>
             </li>
         </ul>
 </template>
@@ -17,4 +17,15 @@
     onMounted(async() => {
         basked.value = await JSON.parse(localStorage.getItem('basked'))
     })
+
+    const removeBasked = (e) => {
+        const id = e.target.closest('.products-list__item').id;
+        basked.value = basked.value.filter(e => e._id !== id)
+        localStorage.setItem('basked', JSON.stringify(basked.value))
+    }
+
+    const removeAllBasked = () => {
+        localStorage.setItem('basked', JSON.stringify([]))
+        basked.value = []
+    }
 </script>
--- pages/favorites/index.vue
+++ pages/favorites/index.vue
@@ -1,11 +1,13 @@
 <template>
     <h1>Sevimlilar</h1>
+    <button @click="removeAllFavorites">Barchasini O'chirish</button>
     <ul class="products-list">
             <li v-for="product in favorites" class="products-list__item" :key="product._id" :id="product._id">
                 <img class="products-list__item-img" :src="product.product_img[0]" width="200" height="200"/>
                 <h3>{{ product.product_title }}</h3>
                 <p>{{ product.product_desc }}</p>
                 <p v-if="product.product_price > 0"><b>Price: </b>{{ product.product_price }} so'm</p>
+                <button @click="removeFavorites">Ochirish</button>
             </li>
         </ul>
 </template>
@@ -15,4 +17,15 @@
     onMounted(async() => {
         favorites.value = await JSON.parse(localStorage.getItem('favorites'))
     })
+
+    const removeFavorites = (e) => {
+        const id = e.target.closest('.products-list__item').id;
+        favorites.value = favorites.value.filter(e => e._id !== id)
+        localStorage.setItem('favorites', JSON.stringify(favorites.value))
+    }
+
+    const removeAllFavorites = () => {
+        localStorage.setItem('favorites', JSON.stringify([]))
+        favorites.value = []
+    }
 </script>
