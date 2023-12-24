1501268c2751c32a4aa335d82bb4d959510f8581
FotihYusupov <yusupovfotih158@gmai.com>
2023-12-24 19:46:23 +0500

    add basked hover fn

--- store/products.js
+++ store/products.js
@@ -15,16 +15,15 @@ export const useProductsStore = defineStore({
       try {
         this.data.loading = true;
         const response = await axios.get('https://gh-server-83lb.onrender.com/api/products');
-        if(response.status == 200) {
+        if (response.status === 200) {
           if (typeof localStorage !== 'undefined') {
-            const basked = JSON.parse(localStorage.getItem('basked'))
-            const favorites = JSON.parse(localStorage.getItem('favorites'))
+            const basked = JSON.parse(localStorage.getItem('basked'));
+            const favorites = JSON.parse(localStorage.getItem('favorites'));
             response.data.forEach((product) => {
-                product.inCart = basked.some((item) => item._id === product._id);
-                if(basked.some((item) => item._id === product._id)) {
-                  product.count = 1
-                }
-                product.inFavorites = favorites.some((item) => item._id === product._id);
+              const cartItem = basked.find((item) => item._id === product._id);
+              product.inCart = !!cartItem;
+              product.count = cartItem ? cartItem.count : 0;
+              product.inFavorites = favorites.some((item) => item._id === product._id);
             });
             this.data.products = response.data;
             this.data.loading = false;
@@ -32,10 +31,10 @@ export const useProductsStore = defineStore({
         } else {
           this.data.error = true;
         }
-      } catch(err) {
-        console.log('Error: ', err)
+      } catch (err) {
+        console.log('Error: ', err);
       }
-    },
+    },    
     updateProductInFavorites(productId, value) {
       const productIndex = this.data.products.findIndex(product => product._id === productId);
       if (productIndex !== -1) {
