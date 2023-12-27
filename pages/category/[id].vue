c2e3dbffbbe26bd94f53d5ec4a2cd1fc2f224253
FotihYusupov <yusupovfotih158@gmai.com>
2023-12-27 12:56:31 +0500

    categories bug fixed

--- pages/category/[id].vue
+++ pages/category/[id].vue
@@ -8,35 +8,37 @@
           <input type="text">
         </div>
       </div> -->
-      <!-- <div v-if="filteredProducts[0]" class="categories-header">
-        <span>
-          <h2>{{ filteredProducts[0].category.category_name }}</h2>
-          <p>{{ filteredProducts.length }} items</p>
-        </span>
-        <span>
-          <p>sort by</p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
-            <g clip-path="url(#clip0_523_13125)">
-              <path d="M13.8136 3.86819C13.5651 3.61971 13.1622 3.61967 12.9137 3.86824L7.00015 9.78188L1.08634 3.86819C0.837858 3.61971 0.434912 3.61967 0.186391 3.86824C-0.0621303 4.11676 -0.0621303 4.51966 0.186391 4.76818L6.5502 11.1318C6.66954 11.2512 6.83138 11.3182 7.00015 11.3182C7.16891 11.3182 7.3308 11.2511 7.4501 11.1318L13.8136 4.76814C14.0621 4.51966 14.0621 4.11671 13.8136 3.86819Z" fill="#AFB0B4"/>
-            </g>
-            <defs>
-              <clipPath id="clip0_523_13125">
-                <rect width="14" height="14" fill="white" transform="translate(0 0.5)"/>
-              </clipPath>
-            </defs>
-        </svg>
-        </span>
-      </div> -->
-      <ul class="categories-list">
-        <card v-for="product in filteredProducts.slice(0,end)" :id="product._id"
-            v-bind:key="product._id" :title="product.product_title" :img="product.product_img"
-            :description="product.product_desc" :views="product.views" :price="product.product_price"
-            :newPrice="product.new_price"
-            :inCart="product.inCart" :inFavorites="product.inFavorites" />
-      </ul>
-      <button class="show-btn" :disabled="filteredProducts.length <= end" @click="clickFn">Show more</button>
-      <p></p>
-    </div>
+      <!-- <span>
+        <p>sort by</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
+          <g clip-path="url(#clip0_523_13125)">
+            <path d="M13.8136 3.86819C13.5651 3.61971 13.1622 3.61967 12.9137 3.86824L7.00015 9.78188L1.08634 3.86819C0.837858 3.61971 0.434912 3.61967 0.186391 3.86824C-0.0621303 4.11676 -0.0621303 4.51966 0.186391 4.76818L6.5502 11.1318C6.66954 11.2512 6.83138 11.3182 7.00015 11.3182C7.16891 11.3182 7.3308 11.2511 7.4501 11.1318L13.8136 4.76814C14.0621 4.51966 14.0621 4.11671 13.8136 3.86819Z" fill="#AFB0B4"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_523_13125">
+              <rect width="14" height="14" fill="white" transform="translate(0 0.5)"/>
+            </clipPath>
+          </defs>
+      </svg>
+      </span> -->
+      <div class="categories-wrapper">
+        <div v-if="filteredProducts[0]" class="categories-header">
+          <span>
+            <h2>{{ filteredProducts[0].category.category_name }}</h2>
+            <p>{{ filteredProducts.length }} items</p>
+          </span>
+        </div>
+        <ul class="list-category">
+          <card v-for="product in filteredProducts.slice(0,end)" :id="product._id"
+              v-bind:key="product._id" :title="product.product_title" :img="product.product_img"
+              :description="product.product_desc" :views="product.views" :price="product.product_price"
+              :newPrice="product.new_price"
+              :inCart="product.inCart" :inFavorites="product.inFavorites" />
+        </ul>
+        <button class="show-btn" :disabled="filteredProducts.length <= end" @click="clickFn">Show more</button>
+        <p></p>
+      </div>
+      </div>
   </template>
 
 <script setup>
