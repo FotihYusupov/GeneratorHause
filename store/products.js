e1500aa53fe70903fe799757ca39aeb1aeb68996
FotihYusupov <yusupovfotih158@gmai.com>
2024-01-01 20:51:52 +0500

    bug fixed

--- store/products.js
+++ store/products.js
@@ -17,14 +17,21 @@ export const useProductsStore = defineStore({
         const response = await axios.get('https://gh-server-83lb.onrender.com/api/products');
         if (response.status === 200) {
           if (typeof localStorage !== 'undefined') {
-            const basked = JSON.parse(localStorage.getItem('basked'));
-            const favorites = JSON.parse(localStorage.getItem('favorites'));
+            let basked = JSON.parse(localStorage.getItem('basked'));
+            let favorites = JSON.parse(localStorage.getItem('favorites'));
+            if(!basked || !favorites) {
+              localStorage.setItem('favorites', JSON.stringify([]));
+              localStorage.setItem('basked', JSON.stringify([]));
+              basked = []
+              favorites = []
+            }
             response.data.forEach((product) => {
               const cartItem = basked.find((item) => item._id === product._id);
               product.inCart = !!cartItem;
               product.count = cartItem ? cartItem.count : 0;
               product.inFavorites = favorites.some((item) => item._id === product._id);
             });
+            console.log(response.data, 'data');
             this.data.products = response.data;
             this.data.loading = false;
           }
@@ -32,7 +39,7 @@ export const useProductsStore = defineStore({
           this.data.error = true;
         }
       } catch (err) {
-        console.log('Error: ', err);
+        console.log('Error: Store', err);
       }
     },    
     updateProductInFavorites(productId, value) {
