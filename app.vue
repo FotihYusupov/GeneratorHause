7fa234ea3a806cac02e789f0f9d72ee4c24a6df6
FotihYusupov <yusupovfotih158@gmai.com>
2024-03-09 11:56:37 +0500

    favicon.ico

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@
       <Meta name="yandex-verification" content="4adbf28d4115d8b8" />
       <Meta name="google-site-verification" content="_GCrBB-LPH-dnA6CJFQ1zfPZfPIUDK5bpaNWBXKLr5A" />
       <Link rel="canonical" href="https://generatorhouse.uz/"/>
-      <Link rel="icon" type="image/x-icon" href="https://generatorhouse.uz/logotip.png"/>
+      <Link rel="icon" type="image/x-icon" href="/favicon.ico"/>
     </Head>
     <NuxtLayout :name="route.fullPath.slice(1, 3) == 'ru' ? 'russian' : 'default'">
       <NuxtPage />
