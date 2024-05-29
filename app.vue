1f3393957c4480833ac84c0dbdf4ec96c318fcac
FotihYusupov <yusupovfotih158@gmai.com>
2024-05-29 15:46:22 +0500

    Akbaraka

--- app.vue
+++ app.vue
@@ -1,23 +1,30 @@
 <template>
-    <Head>
-      <Title>GENERATOR HOUSE</Title>
-      <Meta name="description" content=""/>
-      <Meta property="og:title" content="Genarator House"/>
-      <Meta property="og:description" content="Generator House official web page"/>
-      <Meta property="og:url" content="https://generatorhouse.uz/"/>
-      <Meta property="og:type" content="website"/>
-      <Meta name="keywords" content="Genarator House, House, Genarator, Karcher, Gazli Generator, Stabilizator, Benzinli Genarator, Betonameshalka, Dizel Genarator, Inverter Genarator, Генераторный Дом, Генератор, Инверторный генератор, Дизельный генератор, Бетонамешалка, Бензиновый Генератор, Стабилизатор, Газовый генератор"/>
-      <Meta name="rating" content="Family-Friendly"/>
-      <Meta name="yandex-verification" content="4adbf28d4115d8b8" />
-      <Meta name="google-site-verification" content="_GCrBB-LPH-dnA6CJFQ1zfPZfPIUDK5bpaNWBXKLr5A" />
-      <Link rel="canonical" href="https://generatorhouse.uz/"/>
-      <Link rel="icon" type="image/x-icon" href="/favicon.ico"/>
-    </Head>
-    <NuxtLayout :name="route.fullPath.slice(1, 3) == 'ru' ? 'russian' : 'default'">
-      <NuxtPage />
-    </NuxtLayout>
+
+  <Head>
+    <Title>GENERATOR HOUSE</Title>
+    <Meta name="description" content="Genarator House, House, Genarator, Karcher, Gazli Generator, Stabilizator, Benzinli Genarator, Betonameshalka, Dizel Genarator, Inverter Genarator, Генераторный Дом, Генератор, Инверторный генератор, Дизельный генератор, Бетонамешалка, Бензиновый Генератор, Стабилизатор, Газовый генератор" />
+    <Meta name="keywords" content="Genarator House, House, Genarator, Karcher, Gazli Generator, Stabilizator, Benzinli Genarator, Betonameshalka, Dizel Genarator, Inverter Genarator, Генераторный Дом, Генератор, Инверторный генератор, Дизельный генератор, Бетонамешалка, Бензиновый Генератор, Стабилизатор, Газовый генератор" />
+    <Meta name="rating" content="Family-Friendly" />
+    <Link rel="canonical" href="https://generatorhouse.uz/" />
+    <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <Meta property="og:type" content="business.business" />
+    <Meta property="og:title" content="GENERATOR HOUSE" />
+    <Meta property="og:url" content="https://generatorhouse.uz/" />
+    <Meta property="og:image" content="https://generatorhouse.uz/favicon.ico" />
+    <Meta property="og:description" content="Genarator House, House, Genarator, Karcher, Gazli Generator, Stabilizator, Benzinli Genarator, Betonameshalka, Dizel Genarator, Inverter Genarator, Генераторный Дом, Генератор, Инверторный генератор, Дизельный генератор, Бетонамешалка, Бензиновый Генератор, Стабилизатор, Газовый генератор" />
+    <Meta property="business:contact_data:street_address" content="Xalqa Avtomabil Yo'li" />
+    <Meta property="business:contact_data:locality" content="Toshkent" />
+    <Meta property="business:contact_data:region" content="Toshkent" />
+    <Meta property="business:contact_data:postal_code" content="100000" />
+    <Meta property="business:contact_data:country_name" content="Uzbekistan" />
+    <Meta name="yandex-verification" content="4adbf28d4115d8b8" />
+    <Meta name="google-site-verification" content="_GCrBB-LPH-dnA6CJFQ1zfPZfPIUDK5bpaNWBXKLr5A" />
+  </Head>
+  <NuxtLayout :name="route.fullPath.slice(1, 3) == 'ru' ? 'russian' : 'default'">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <script setup>
-const route = useRoute()
+const route = useRoute();
 </script>
